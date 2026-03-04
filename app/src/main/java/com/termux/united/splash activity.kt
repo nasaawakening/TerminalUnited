3c3794b2aa8a86e02ad7a988a9819bab2142de62
nasaawakening <nasaawakening@gmail.com>
2026-03-04 13:57:04 +0700

    Change package name and modify splash delay
    Updated package name and adjusted splash screen delay.

--- app/src/main/java/com/termux/united/splash activity.kt
+++ app/src/main/java/com/termux/united/splash activity.kt
@@ -1,4 +1,4 @@
-package com.terminal.united
+package com.TerminalUnited
 
 import android.content.Intent
 import android.os.Bundle
@@ -9,12 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+        // Set theme sebelum super.onCreate kalau mau ubah theme runtime
+        setTheme(R.style.Theme_TerminalUnited_NoActionBar)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2500) // 2.5 detik
+        }, 2000)
     }
 }
