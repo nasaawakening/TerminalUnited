0d75303c41d73b168dc148fb184cbeba87bf9e98
nasaawakening <nasaawakening@gmail.com>
2026-03-05 19:46:35 +0700

    Create SplashActivity.kt

---
+++ app/src/main/java/com/termux/united/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.Terminalunited
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        
+        // Set theme sebelum super.onCreate kalau mau ubah theme runtime
+        setTheme(R.style.Theme_TerminalUnited_NoActionBar)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
