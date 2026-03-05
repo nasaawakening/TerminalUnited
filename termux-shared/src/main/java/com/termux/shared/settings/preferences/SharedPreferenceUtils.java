f894007a7ae7ebc76dc05354558424ec98eff6a2
nasaawakening <nasaawakening@gmail.com>
2026-03-05 23:37:11 +0700

    Update SharedPreferenceUtils.java

--- termux-shared/src/main/java/com/termux/shared/settings/preferences/SharedPreferenceUtils.java
+++ termux-shared/src/main/java/com/termux/shared/settings/preferences/SharedPreferenceUtils.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.terminalunited.shared.logger.Logger;
+import com.termux.shared.logger.Logger;
 
 import java.util.Set;
 
