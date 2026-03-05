285d0d1763e8491e2e710e2688ef4c82e21f29df
nasaawakening <nasaawakening@gmail.com>
2026-03-06 06:05:28 +0700

    Update FunctionErrno.java

--- termux-shared/src/main/java/com/termux/shared/errors/FunctionErrno.java
+++ termux-shared/src/main/java/com/termux/shared/errors/FunctionErrno.java
@@ -1,4 +1,4 @@
-package com.terminalunited.shared.errors;
+package com.termux.shared.errors;
 
 /** The {@link Class} that defines function error messages and codes. */
 public class FunctionErrno extends Errno {
