e3c71c10849dc3048e8a81cb6f109d8b73255e41
nasaawakening <nasaawakening@gmail.com>
2026-03-05 23:34:06 +0700

    Update LocalSocketManagerClientBase.java

--- termux-shared/src/main/java/com/termux/shared/net/socket/local/LocalSocketManagerClientBase.java
+++ termux-shared/src/main/java/com/termux/shared/net/socket/local/LocalSocketManagerClientBase.java
@@ -3,8 +3,8 @@ package com.terminalunited.shared.net.socket.local;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.terminalunited.shared.errors.Error;
-import com.terminalunited.shared.logger.Logger;
+import com.termux.shared.errors.Error;
+import com.termux.shared.logger.Logger;
 
 /** Base helper implementation for {@link ILocalSocketManager}. */
 public abstract class LocalSocketManagerClientBase implements ILocalSocketManager {
