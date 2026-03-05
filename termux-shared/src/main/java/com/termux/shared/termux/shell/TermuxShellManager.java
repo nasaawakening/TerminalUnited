9c74f4f0b9a88ae029928c9596ee5af76719c057
nasaawakening <nasaawakening@gmail.com>
2026-03-06 04:09:05 +0700

    Update TermuxShellManager.java

--- termux-shared/src/main/java/com/termux/shared/termux/shell/TermuxShellManager.java
+++ termux-shared/src/main/java/com/termux/shared/termux/shell/TermuxShellManager.java
@@ -6,10 +6,10 @@ import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 
-import com.terminalunited.shared.shell.command.ExecutionCommand;
-import com.terminalunited.shared.shell.command.runner.app.AppShell;
-import com.terminalunited.shared.termux.settings.preferences.TermuxAppSharedPreferences;
-import com.terminalunited.shared.termux.shell.command.runner.terminal.TermuxSession;
+import com.termux.shared.shell.command.ExecutionCommand;
+import com.termux.shared.shell.command.runner.app.AppShell;
+import com.termux.shared.termux.settings.preferences.TermuxAppSharedPreferences;
+import com.termux.shared.termux.shell.command.runner.terminal.TermuxSession;
 
 import java.util.ArrayList;
 import java.util.List;
