622c6dd2c6b1490c84533fb37fb8665d81d28777
nasaawakening <nasaawakening@gmail.com>
2026-03-05 23:16:39 +0700

    Update PackageUtils.java

--- termux-shared/src/main/java/com/termux/shared/android/PackageUtils.java
+++ termux-shared/src/main/java/com/termux/shared/android/PackageUtils.java
@@ -16,11 +16,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
-import com.terminalunited.shared.R;
-import com.terminalunited.shared.data.DataUtils;
-import com.terminalunited.shared.interact.MessageDialogUtils;
-import com.terminalunited.shared.logger.Logger;
-import com.terminalunited.shared.reflection.ReflectionUtils;
+import com.termux.shared.R;
+import com.termux.shared.data.DataUtils;
+import com.termux.shared.interact.MessageDialogUtils;
+import com.termux.shared.logger.Logger;
+import com.termux.shared.reflection.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.security.MessageDigest;
